333ffcd4bbc7800dac787acd544d453dcbcaf2b2
Bia <95707803+biachalegre@users.noreply.github.com>
2024-04-18 20:54:37 -0300

    revert: Remove o pacote dynamoose e as configurações de servidor.
    Retorna para o commit 4bfd31b9a3ae782f4a98917854176666977936ff.

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-const express = require("express");
-const morgan = require("morgan");
-const routes = require("./src/routes");
-const app = express();
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-app.use(routes);
-app.listen(3000, () => console.log("Ok"));
+const pup = require("puppeteer");
+
+const url = "https://www.mercadolivre.com.br/";
+const searchFor = "macbook";
+
+let pageNumber = 1;
+
+(async () => {
+  const browser = await pup.launch({ headless: false });
+  const page = await browser.newPage();
+  console.log("Navegador aberto");
+
+  await page.goto(url);
+  console.log("Página aberta");
+
+  await page.waitForSelector("#cb1-edit");
+
+  await page.type("#cb1-edit", searchFor);
+
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.click(".nav-search-btn"),
+  ]);
+
+  const links = await page.$$eval(".ui-search-item__group > a", (el) =>
+    el.map((link) => link.href)
+  );
+
+  // console.log(links);
+
+  for (const link of links) {
+    if (pageNumber === 10) continue;
+    await page.goto(link);
+    await page.waitForSelector(".ui-pdp-title");
+
+    const title = await page.$eval(
+      ".ui-pdp-title",
+      (element) => element.innerText
+    );
+
+    const price = await page.$eval(
+      ".andes-money-amount__fraction",
+      (element) => element.innerText
+    );
+
+    const obj = { title, price };
+    console.log(obj);
+
+    pageNumber++;
+  }
+
+  setTimeout(async () => {
+    await browser.close();
+  }, 3000);
+})();
